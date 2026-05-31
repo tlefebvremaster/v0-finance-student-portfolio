fe922581fcbe976292adf21e4acffac0532d1e52
tlefebvremaster <tlefebvre@eugeniaschool.com>
2026-05-31 18:00:56 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,6 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
- 
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  output: 'export',
+  basePath: '/v0-finance-student-portfolio',
+  images: { unoptimized: true },
 }
-
 export default nextConfig
